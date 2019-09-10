367f9936809a5f054c0129dfeabd107688db9615
Akira Shinohara <k017c1067@it-neec.jp>
2019-09-11 00:25:14 +0900

    [new] note schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,4 +4,12 @@ const dbName = 'moja-note';
 const version = 1;
 const schemaBuilder = lf.schema.create(dbName, version);
 
+schemaBuilder.createTable('note').
+  addColumn('id', lf.Type.INTEGER).
+  addColumn('title', lf.Type.STRING).
+  addColumn('content', lf.Type.STRING).
+  addColumn('created_at', lf.Type.DATE_TIME).
+  addColumn('updated_at', lf.Type.DATE_TIME).
+  addPrimaryKey(['id'], true);
+
 export default schemaBuilder;
