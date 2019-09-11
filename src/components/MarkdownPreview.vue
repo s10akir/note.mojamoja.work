5854f0b7afd3324e6377672bac84adf61466273c
tatikaze <k017c1356@it-neec.jp>
2019-09-11 21:25:19 +0900

    [new] markdown preview dummy

---
+++ src/components/MarkdownPreview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="preview">
+    <h1>{{title}}</h1>
+    <p>{{content}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MarkdownPreview',
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
