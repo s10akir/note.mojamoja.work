192c1b43fd239d7cc00e3936a919cf95e7f3fd97
Akira Shinohara <k017c1067@it-neec.jp>
2019-09-25 21:24:43 +0900

    [add] render markdown to html

--- src/components/MarkdownPreview.vue
+++ src/components/MarkdownPreview.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="preview">
     <h1>{{ title }}</h1>
-    <p>{{ content }}</p>
+    <p v-html="render(content)" />
   </div>
 </template>
 
 <script>
+import marked from "marked";
+
 export default {
   name: "MarkdownPreview",
   props: {
     title: String,
     content: String
+  },
+  methods: {
+    render: text => {
+      return marked(text);
+    }
   }
 };
 </script>
