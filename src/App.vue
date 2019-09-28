b4cdc43af6daa4020815cffdb2205d39391f6327
tatikaze <k017c1356@it-neec.jp>
2019-09-28 09:05:36 +0000

    [delete] delete header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text to="/note/1/edit">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
       <router-view />
     </v-content>
