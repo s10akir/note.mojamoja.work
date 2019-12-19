eb113de387eb8be7fe0dc4e6a5e81060607b8e65
Akira Shinohara <k017c1067@it-neec.jp>
2019-12-19 11:10:14 +0900

    [add] persistence editor setting

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -51,12 +51,21 @@ export default {
       .exec()
       .then(result => (this.note = result[0]));
   },
+  mounted() {
+    // 初回起動時はエディタ設定が存在しない
+    if (localStorage.cmOption) {
+      // 前回設定読み込み
+      this.cmOption = JSON.parse(localStorage.cmOption);
+    } else {
+      // デフォルト値
+      localStorage.cmOption = JSON.stringify(this.cmOption);
+    }
+  },
   data: () => ({
     note: {
       title: "",
       content: ""
     },
-    // この辺をユーザが任意で設定できるユーティリティを提供する
     cmOption: {
       connect: "align",
       mode: "text/markdown",
@@ -89,6 +98,13 @@ export default {
       // autoSaveIntervalで指定された秒数更新がなかった場合にノートを保存する
       clearInterval(saveTimer);
       saveTimer = setTimeout(this.saveNote, this.autoSaveInterval, value);
+    },
+    cmOption: {
+      handler() {
+        console.log(this.cmOption);
+        localStorage.cmOption = JSON.stringify(this.cmOption);
+      },
+      deep: true
     }
   }
 };
